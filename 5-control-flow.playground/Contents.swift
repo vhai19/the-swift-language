72bf26dfdaed398ee794a9ff22e824e70d3dabf5
Nguyen Van Hai <n.vanhai19@gmail.com>
2018-07-28 23:18:17 +0900

    [Swift Programming Language]feat control-flow

---
+++ 5-control-flow.playground/Contents.swift
@@ -0,0 +1,83 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+// FOR-IN
+// WHILE
+
+//let finalSquare = 25
+//var board = [Int](repeating: 0, count: finalSquare + 1)
+//
+//board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+//board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+//
+//var square = 0
+//var  diceRoll = 0
+//
+//while square < finalSquare {
+//    // roll the dice
+//    diceRoll += 1
+//    if diceRoll == 7 {diceRoll = 1}
+//
+//    // move by the rolled amount
+//    square += diceRoll
+//    if square < board.count {
+//        square += board[square]
+//    }
+//}
+//print("Game Over!")
+
+// SWITCH
+let anotherCharacter: Character = "a"
+switch anotherCharacter {
+case "a", "A":
+    print("The letter A")
+default:
+    print("Not the letter A")
+}
+
+let approximateCount = 62
+let countedThings = "moons orbiting Saturn"
+let naturalCount: String
+switch approximateCount {
+case 0:
+    naturalCount = "no"
+case 1..<5:
+    naturalCount = "a few"
+case 5..<12:
+    naturalCount = "several"
+case 12..<100:
+    naturalCount = "dozens of"
+case 100..<1000:
+    naturalCount = "hundreds of"
+default:
+    naturalCount = "many"
+}
+print("There are \(naturalCount) \(countedThings).")
+
+let somePoint = (2, 1)
+switch somePoint {
+case (0, 0):
+    print("\(somePoint) is at the origin")
+case (_, 0):
+    print("\(somePoint) is on the x-axis")
+case (0, _):
+    print("\(somePoint) is on the y-axis")
+case (-2...2, -2...2):
+    print("\(somePoint) is inside the box")
+default:
+    print("\(somePoint) is outside of the box")
+}
+
+let anotherPoint = (4, 0)
+switch anotherPoint {
+case (let x, 0):
+    print("on the x-axis with an x value of \(x)")
+case (0, let y):
+    print("on the y-axis with a y value of \(y)")
+case let (x, y):
+    print("somewhere else at (\(x), \(y))")
+}
